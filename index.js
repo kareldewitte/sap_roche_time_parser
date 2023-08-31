c51a518c96d0359b251dc5f44ea25f05b3d1a8a9
Karel De Witte <karel.de_witte@roche.com>
2023-08-31 17:35:03 +0200

    adding copy paste functionality

--- index.js
+++ index.js
@@ -2,6 +2,48 @@ import Plotly from 'plotly.js-dist-min'
 import './css/input.css';
 import './webcomponents/list-card.js';
 
+let times_global={};
+
+
+function merge(old,nw){
+  old.OFF += nw.OFF;
+  old.ONSITE += nw.ONSITE;
+  old.WFH += nw.WFH;
+  old.TRAVEL += nw.TRAVEL;
+  old.REM += nw.REM;
+  return old;
+}
+
+function reorg(objectArray) {
+  return objectArray.reduce(
+    function (accumulator, currentObject,index,array) {
+       
+      if(currentObject && currentObject["day"]=="cont'd"){
+          let prev = array[index-1];
+          if(prev){
+            array[index-1]=merge(prev,currentObject);
+          }
+          delete array[index];
+          //return array;
+        }
+      if(currentObject==undefined)delete array[index];
+       return array;
+      }, {});
+}
+
+function remap(objectArray) {
+  return objectArray.map(
+    function (x) {
+      let obj = {'day':x.day,'WFH':0,'ONSITE':0,'REM':0,'TRAVEL':0,'OFF':0}
+      if(x.timeType!="NA"){
+        obj[x.timeType]=x.time;
+        return obj;
+      }
+    }
+  )
+}
+
+
 
 import('./pkg')
   .then(rust_module => {
@@ -42,28 +84,36 @@ import('./pkg')
             
       Plotly.newPlot('diagram', data, layout);
       const details = document.querySelector("#rows");
-      
-      json_res.times.reverse().forEach(x => {
-        console.log(x);
-        details.insertAdjacentHTML(
-          'afterend',
-          "<div class='table-row text-xs text-gray-700 text-center'>"+
-          "<div class='table-cell'>"+x.day+"</div>"+
-          "<div class='table-cell'>"+x.timeType+"</div>"+
-          "<div class='table-cell'>"+x.time+"</div>"
+     
+      let times = reorg(remap(json_res.times));
+      let wfh_times = [],onsite_times=[],travel_times=[];
 
-          +"</div>",
-        );
+      times.reverse().forEach(x => {
+                if(x.day!='na'){
+                  
+                  let onsite = Math.round((x.ONSITE+x.REM+Number.EPSILON)*100)/100;
+                  details.insertAdjacentHTML(
+                    'afterend',
+                    "<div class='table-row text-xs text-gray-700 text-center'>"+
+                    "<div class='table-cell'>"+x.day+"</div>"+
+                    "<div class='table-cell'>"+x.WFH+"</div>"+
+                    "<div class='table-cell'>"+onsite+"</div>"+
+                    "<div class='table-cell'>"+x.TRAVEL+"</div></div>",
+                  );
+                  wfh_times.push(x.WFH);
+                  onsite_times.push(onsite);   
+                  travel_times.push(x.TRAVEL);  
+              }
 
         });
-      
-      
-
-    
+       
+        sessionStorage.setItem("wfh_times",wfh_times.reverse().join("\n"));
+        sessionStorage.setItem("onsite_times",onsite_times.reverse().join("\n"));
+        sessionStorage.setItem("travel_times",travel_times.reverse().join("\n"));
     
     }
 
-
+    
 
 
 
